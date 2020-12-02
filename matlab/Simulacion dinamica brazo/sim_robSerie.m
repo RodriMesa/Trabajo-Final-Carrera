c966c70caa0550a452863b580e242de3f1be593f
RodriMesa <60247106+RodriMesa@users.noreply.github.com>
2020-12-02 18:27:34 -0300

    Cambios y mejoras

--- matlab/Simulacion dinamica brazo/sim_robSerie.m
+++ matlab/Simulacion dinamica brazo/sim_robSerie.m
@@ -3,6 +3,10 @@ robot = importrobot('..\Simulacion dinamica brazo\Modelos urdf brazo\Robot\urdf\
 %robot = importrobot('..\Simulacion dinamica brazo\Modelos urdf brazo\Brazo\urdf\Brazo.urdf');
 robot.DataFormat = 'column';
 robot.Gravity=[0 0 -9.81];
+robot.Bodies{1,1}.Joint.PositionLimits=[-180,180];
+robot.Bodies{1,2}.Joint.PositionLimits=[-180,180];
+robot.Bodies{1,3}.Joint.PositionLimits=[-180,180];
+robot.Bodies{1,4}.Joint.PositionLimits=[-180,180];
 %%
 % Simulación de cinemática inversa
 t = (0:0.01 :10)'; % Time
@@ -30,9 +34,15 @@ waypoints = [0.011265502281722 0.032831860285015   -1.078889510983337;
     0.4 -0.4  -0.8;
     -0.4 -0.4  -0.8;
     -0.4 0.4  -0.8];
+
+% waypoints = [0.011265502281722 0.032831860285015   -1.078889510983337;
+%     0.3 0.1 -0.8;
+%     0.2 -0.5  -0.9;
+%     -0.2 -0.4  -0.8;
+%     -0.4 0.4  -0.8];
 waypoints = waypoints';
 timepoints = [0 0.5 1 1.5 2];
-t = 0:0.1:2;
+t = 0:0.001:2;
 q =quinticpolytraj(waypoints,timepoints,t);
 
 posx = timeseries(q(1,:),t);
@@ -40,24 +50,32 @@ posy = timeseries(q(2,:),t);
 posz = timeseries(q(3,:),t);
 
 %% Planificador de trayectorias interpolador articular
-% waypoints = [-0.0330 0.0338   -1.0770;
-%     0.4 0.4 -0.8;
-%     0 -0.3  -0.5];
-% t1 = trvec2tform(waypoints(1,:));
-% t2 = trvec2tform(waypoints(2,:));
-% t3 = trvec2tform(waypoints(3,:));
-% qSol1 = ik(endEffector,t1,weights,q0);
-% qSol2 = ik(endEffector,t2,weights,qSol1);
-% 
-% waypoints = [q0 qSol1 qSol2];
-% timepoints = [0 1 2];
-% t = 0:0.1:2;
-% q =quinticpolytraj(waypoints,timepoints,t);
-% q1 = timeseries(q(1,:),t);
-% q2 = timeseries(q(2,:),t);
-% q3 = timeseries(q(3,:),t);
-% q4 = timeseries(q(4,:),t);
+waypoints = [0.011265502281722 0.032831860285015   -1.078889510983337;
+    0 0.5 -0.8;
+    0 0.5  -0.6;
+    0 -0.5  -0.6;
+    0 -0.5 -0.8];
+qSol=qInitial;
+qsols = zeros(4, 4);
+qsols(1,:)=qSol';
+for i = 2:length(waypoints(:,1))
+    qSol = ik(endEffector,trvec2tform(waypoints(i,:)),weights,qInitial);
+    % Store the configuration
+    qsols(i,:) = qSol';
+    % Start from prior solution
+    qInitial = qSol;
+end
+timepoints = [0 0.5 1 1.5 2];
+[q22,q2p,q2pp] =quinticpolytraj(qsols',timepoints,t);
+q2 = timeseries(q22',t');
+punt_art = zeros(length(q22(1,:)),3);
 
+for i=1:length(q22(1,:))
+    transform = getTransform(robot,q22(:,i),'tool','base_link');
+    punt_art(i,1) = transform(1,4);
+    punt_art(i,2) = transform(2,4);
+    punt_art(i,3) = transform(3,4);
+end
 %% Simulación
 out = sim('robot_serie',t(end));
 
@@ -65,10 +83,11 @@ out = sim('robot_serie',t(end));
 salto = round(length(out.jointData)/60);
 figure(1)
 show(robot,out.jointData(1,:)','Frames','off');
-axis([-1.5 1.5 -1.5 1.5 -1.5 0.3])
+axis([-1.5 1.5 -1.5 1.5 -1.3 0.3])
 hold on
 pause 
-plot3(q(1,:),q(2,:),q(3,:))
+%plot3(q(1,:),q(2,:),q(3,:))
+plot3(punt_art(:,1),punt_art(:,2),punt_art(:,3))
 fps = 30;
 r = rateControl(fps);
 for i = 1:salto:length(out.jointData)
@@ -84,51 +103,100 @@ end
 hold off
 pause
 close all
-figure(1)
+figure(2)
 plot(out.torque1.Time,out.torque1.Data)
 title('Torque nesesario en la articulación 1')
 xlabel('t(s)')
 ylabel('T (N m)')
 grid on
-figure(2)
+figure(3)
 plot(out.torque2.Time,out.torque2.Data)
 title('Torque nesesario en la articulación 2')
 xlabel('t(s)')
 ylabel('T (N m)')
 grid on
-figure(3)
+figure(4)
 plot(out.torque3.Time,out.torque3.Data)
 title('Torque nesesario en la articulación 3')
 xlabel('t(s)')
 ylabel('T (N m)')
 grid on
-figure(4)
+figure(5)
 plot(out.torque4.Time,out.torque4.Data)
 title('Torque nesesario en la articulación 4')
 xlabel('t(s)')
 ylabel('T (N m)')
 grid on
-figure(5)
+figure(6)
 plot(out.tout,out.torque1.Data.*out.velocidades(:,1))
 title('Potencia nesesaria en la articulación 1')
 xlabel('t(s)')
 ylabel('P (W)')
 grid on
-figure(6)
+figure(7)
 plot(out.tout,out.torque2.Data.*out.velocidades(:,2))
 title('Potencia nesesaria en la articulación 2')
 xlabel('t(s)')
 ylabel('P (W)')
 grid on
-figure(7)
+figure(8)
 plot(out.tout,out.torque3.Data.*out.velocidades(:,3))
 title('Potencia nesesaria en la articulación 3')
 xlabel('t(s)')
 ylabel('P (W)')
 grid on
-figure(8)
+figure(9)
 plot(out.tout,out.torque4.Data.*out.velocidades(:,4))
 title('Potencia nesesaria en la articulación 4')
 xlabel('t(s)')
 ylabel('P (W)')
+grid on
+
+figure
+plot(out.torque1.Time,out.torque1.Data*100/9.8)
+title('Torque nesesario en la articulación 1')
+xlabel('t(s)')
+ylabel('T (Kg cm)')
+grid on
+figure
+plot(out.torque2.Time,out.torque2.Data*100/9.8)
+title('Torque nesesario en la articulación 2')
+xlabel('t(s)')
+ylabel('T (Kg cm)')
+grid on
+figure
+plot(out.torque3.Time,out.torque3.Data*100/9.8)
+title('Torque nesesario en la articulación 3')
+xlabel('t(s)')
+ylabel('T (Kg cm)')
+grid on
+figure
+plot(out.torque4.Time,out.torque4.Data*100/9.8)
+title('Torque nesesario en la articulación 4')
+xlabel('t(s)')
+ylabel('T (Kg cm)')
+grid on
+figure
+plot(out.tout,out.velocidades(:,1)*60/2/pi)
+title('Velocidad nesesaria en la articulación 1')
+xlabel('t(s)')
+ylabel('V (rpm)')
+grid on
+figure
+plot(out.tout,out.velocidades(:,2)*60/2/pi)
+title('Velocidad nesesaria en la articulación 2')
+xlabel('t(s)')
+ylabel('V (rpm)')
+grid on
+figure
+plot(out.tout,out.velocidades(:,3)*60/2/pi)
+title('Velocidad nesesaria en la articulación 3')
+xlabel('t(s)')
+ylabel('V (rpm)')
+grid on
+figure
+plot(out.tout,out.velocidades(:,4)*60/2/pi)
+title('Velocidad nesesaria en la articulación 4')
+xlabel('t(s)')
+ylabel('V (rpm)')
 grid on
